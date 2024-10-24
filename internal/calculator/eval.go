5ccb1a1c7ee120a40ac5519f8a9c368bb0c0b75a
philipslstwoyearse <braleosta@mail.ru>
2024-10-24 21:23:04 +0300

    calc project

--- internal/calculator/eval.go
+++ internal/calculator/eval.go
@@ -2,6 +2,7 @@ package calc
 
 import (
 	"fmt"
+	"github.com/philipslstwoyears/calculator-go/internal/stack"
 	"strconv"
 	"strings"
 	"unicode"
@@ -22,7 +23,7 @@ func operate(num1 float64, num2 float64, operation string) float64 {
 }
 
 func calcPolishNotation(polishNotation []string) (float64, error) {
-	numStack := New[float64]()
+	numStack := stack.New[float64]()
 	for _, elem := range polishNotation {
 		switch elem {
 		case "+", "-", "*", "/":
@@ -51,7 +52,7 @@ func calcPolishNotation(polishNotation []string) (float64, error) {
 
 func convertToPolishNotation(expression string) ([]string, error) {
 	polishNotation := []string{}
-	operationStack := New[string]()
+	operationStack := stack.New[string]()
 	num := ""
 	countBrackets := 0
 
@@ -133,46 +134,4 @@ func Calc(expression string) (float64, error) {
 		return -1, err
 	}
 	return result, nil
-}
-
-// stack
-
-type element interface {
-	int | float64 | string
-}
-
-type Stack[T element] struct {
-	items    []T
-	topIndex int
-}
-
-func New[T element]() *Stack[T] {
-	return &Stack[T]{
-		items:    make([]T, 0),
-		topIndex: -1,
-	}
-}
-
-func (s *Stack[T]) IsEmpty() bool {
-	return s.topIndex == -1
-}
-
-func (s *Stack[T]) Push(item T) {
-	s.topIndex++
-	s.items = append(s.items, item)
-}
-
-func (s *Stack[T]) Pop() T {
-	popedElement := s.items[s.topIndex]
-	s.topIndex--
-	s.items = s.items[:s.topIndex+1]
-	return popedElement
-}
-
-func (s *Stack[T]) Peek() T {
-	return s.items[s.topIndex]
-}
-
-func (s *Stack[T]) Size() int {
-	return len(s.items)
 }
