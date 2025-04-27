bfa65a9c82693ee34fe8a23ba30a03d3bc8886bd
philipslstwoyearse <braleosta@mail.ru>
2025-04-27 19:06:37 +0300

    proto

--- internal/agent/handlers.go
+++ internal/agent/handlers.go
@@ -1,9 +1,13 @@
 package agent
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/philipslstwoyears/calculator-go/internal/dto"
+	"github.com/philipslstwoyears/calculator-go/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net/http"
 	"strconv"
 )
@@ -53,4 +57,45 @@ func (a *Application) ExpressionHandler(w http.ResponseWriter, r *http.Request) 
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(i)
+}
+
+func (a *Application) Calc(ctx context.Context, r *proto.Request) (*proto.Id, error) {
+	expression := dto.Expression{
+		Expression: r.Expression,
+		Status:     "Выражение принято для вычисления",
+	}
+	id := a.storage.Add(expression)
+	expression.Id = id
+	a.input <- expression
+	return &proto.Id{
+		Id: int32(id),
+	}, nil
+}
+func (a *Application) GetExpressions(context.Context, *proto.Empty) (*proto.Expressions, error) {
+	exp := a.storage.GetAll()
+	result := make([]*proto.Expression, len(exp))
+	for i, expression := range exp {
+		result[i] = &proto.Expression{
+			Expression: expression.Expression,
+			Status:     expression.Status,
+			Id:         int32(expression.Id),
+			Result:     float32(expression.Result),
+		}
+	}
+	return &proto.Expressions{
+		Expressions: result,
+	}, nil
+}
+func (a *Application) GetExpression(ctx context.Context, id *proto.Id) (*proto.Expression, error) {
+	i, ok := a.storage.Get(int(id.GetId()))
+	if !ok {
+		return nil, status.Error(codes.NotFound, "expression not found")
+	}
+	expression := &proto.Expression{
+		Expression: i.Expression,
+		Status:     i.Status,
+		Result:     float32(i.Result),
+		Id:         int32(i.Id),
+	}
+	return expression, nil
 }
